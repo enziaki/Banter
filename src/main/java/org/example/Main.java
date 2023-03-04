82572fb86af528a628356559b91c987b8aff35b9
Enziaki <58030340+enziaki@users.noreply.github.com>
2023-03-05 04:31:36 +0530

    Idea's refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,32 +8,34 @@ import java.net.Socket;
 
 import org.example.FileHandling.*;
 import org.example.Help.Helper;
-public class Main {
-  private static DataOutputStream dataOutput= null;
-  private static DataInputStream dataInput = null;
-  public static void main(String[] args) throws IOException{
-  // defining a port
-  int port = 6969;
-   if (args.length != 3){
-     Helper.help();
-   }
-   // Client side sendFile block
-   if (args[0].equals("--send")){
-     try(Socket clsock = new Socket(args[2], port)){
-       dataInput = new DataInputStream(clsock.getInputStream());
-       dataOutput = new DataOutputStream(clsock.getOutputStream());
-       System.out.println("Initiated!");
 
-       // calling the SendFile method
-       SendFile.sendFile(args[1], dataOutput);
-       dataInput.close();
-       dataOutput.close();
-     }catch (Exception e){
-       e.printStackTrace();
-     }
-    // Server side recvFile method
-   } else if(args[0].equals("--recv")) {
-      try(ServerSocket srvsock = new ServerSocket(port)){
+public class Main {
+  private static DataOutputStream dataOutput = null;
+  private static DataInputStream dataInput = null;
+
+  public static void main(String[] args) throws IOException {
+    // defining a port
+    int port = 6969;
+    if (args.length != 3) {
+      Helper.help();
+    }
+    // Client side sendFile block
+    if (args[0].equals("--send")) {
+      try (Socket clsock = new Socket(args[2], port)) {
+        dataInput = new DataInputStream(clsock.getInputStream());
+        dataOutput = new DataOutputStream(clsock.getOutputStream());
+        System.out.println("Initiated!");
+
+        // calling the SendFile method
+        SendFile.sendFile(args[1], dataOutput);
+        dataInput.close();
+        dataOutput.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      // Server side recvFile method
+    } else if (args[0].equals("--recv")) {
+      try (ServerSocket srvsock = new ServerSocket(port)) {
         System.out.println("Using port: " + port + " on addr: " + args[2]);
 
         // accepting client's request
@@ -47,13 +49,12 @@ public class Main {
         dataInput.close();
         dataOutput.close();
         acceptClSock.close();
-      }catch (Exception e){
+      } catch (Exception e) {
         e.printStackTrace();
       }
-     }
-    else{
+    } else {
       System.err.println("Arguments missing!");
       Helper.help();
-   }
+    }
   }
 }
